b5cd49aeabc7f5fe5d1de527f8debf9f6a653b7b
badalkr2004 <badalkr279@gmail.com>
2024-04-09 01:23:05 +0530

    Solved time out issue in /search route

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,16 +1,22 @@
+import { connect } from '@/db/connect'
 import Image from '@/db/image.model'
 import { type NextRequest } from 'next/server'
- 
+
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const query = searchParams.get('query')
-let result
-  if(query){
-       result = await Image.find({subjectName:query})
-  }else{
-    result = await Image.find()
+  let result
+
+  try {
+    await connect()
+    if (query) {
+      result = await Image.find({ subjectName: query })
+      return Response.json({ message: "success", result })
+    } else {
+      result = await Image.find()
+      return Response.json({ message: "success", result })
+    }
+  } catch (error : any) {
+    return Response.json({ message: error.message })
   }
-
-
-  return Response.json({message:"hello from Backend",result})
 }
